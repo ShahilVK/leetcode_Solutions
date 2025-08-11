9d28a9da1a9f208333560f5a48b00e472b0a9abc
ShahilVK <shahilvk99@gmail.com>
2025-08-11 18:06:38 +0530

    Time: 0 ms (100%), Space: 55.2 MB (30.35%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let sum=0;
+    let sum1=0;
+    for(i=1;i<=n;i++){
+        if(i%m!==0){
+            sum+=i;
+        }else{
+            sum1+=i
+        }
+
+    }
+    return sum-sum1
+    
+};
